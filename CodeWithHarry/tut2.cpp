ee1c59bf3b700cc38cbf87cf749f649f04ffde7a
er-hardikverma <116429017+er-hardikverma@users.noreply.github.com>
2022-11-13 15:14:06 +0530

    Create tut2.cpp

---
+++ CodeWithHarry/tut2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+// ("#"=hash)
+//("<,>"=angular braces) (brackets is also called brackets) 
+// "iostream" is header file
+// here number of blank lines do not matter as "C++" ignores blank lines
+// int = integer 
+// main is a entry point. It is kind of a entry function.
+//func=function
+// "(" = open parenthesis 
+// ")" = close parenthesis
+// "{}" = curly braces (used for starting and ending function body.)
+
+int main()
+{
+//main body of this function lies here
+
+ //here "std::" can be used before cout or in starting (using namespace std;) is used. It's main purpose is to define that from which header file or namespace that function belongs to. 
+ // "std" is a type of namespace.
+ 
+  cout<<"Hello World";
+  // ";" it is used to end line.
+  /* we write next instruction in next line because it should loook aesthatically nice(good looking)also it is easy to understand.  */
+  return 0;
+}
