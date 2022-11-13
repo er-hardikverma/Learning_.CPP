fa87e0054a3f753257d18a8a2c79316f4d42caeb
er-hardikverma <116429017+er-hardikverma@users.noreply.github.com>
2022-11-13 15:15:42 +0530

    Create 2)Page5.cpp

---
+++ CodeAcademy/2)Page5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear HardikVerma, It's a mandatory task for you to learn c++ and then further clear your logs and imnprove logical thinking.\n";
+  
+  
+  
+  
+  
+}
